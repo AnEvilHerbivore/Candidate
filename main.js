e8bd36ab7c148875a136bd7c29d23df32f7d16c0
Katheryn Ford <AnEvilHerbivoreLP@gmail.com>
2018-04-06 13:36:36 -0500

    Project complete. Function works, elements added to DOM

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// Object declarations
 const candidate = {name: "Elizabeth Sanger", district: "Tennessee 5th Congressional District", bio: "Bio: Was born in a log cabin.", mission: "Mission: To get elected by any means necessary."}
 
-const platform = {taxes: "Against", jobs: "For", ifrastructure: "For", health: "For", Crime : "Against"}
+const platform = {taxes: "Against taxes", jobs: "For jobs", infrastructure: "For infrastructure", health: "For healthcare", Crime : "Against crime"}
 
 const events = [
-    {name: "Charity Dinner", date: "April 10th", location: "The Moon"},
-    {name: "Illuminati Meeting", date: "April 12th", location: "Wherever they tell us to go"},
+    {name: "Charity Dinner", date: "When: April 10th", location: "Where: The Moon"},
+    {name: "Illuminati Meeting", date: "When: April 12th", location: "Where: Wherever they tell us to go"},
 ]
 
 const volunteers = [
@@ -20,13 +21,13 @@ const images = {
 
 const links = {register: "#", donate: "#"}
 
+//Function updates any object property passed into it with new info passed in
 function update (oToChange, propToChange, newInfo) {
     oToChange[propToChange] = newInfo;
 }
 
-
+// update function called
 update(images, "headShot", "NewPhoto.jpg");
-
 update(volunteers[0], "name", "Chris");
 
 // Adding article to the dom and giving it an attribute
@@ -37,15 +38,62 @@ parent.appendChild(child);
 // setting article as the parent reference
 parent = child;
 
-//adds thev info passed into from the objects to the specified parent using the specified tag
-function addDOM (parent, tag, info) {
+//adds the info passed into from the objects to the specified parent using the specified tag
+function addDOM (tag, info) {
     child = document.createElement(tag);
     child.textContent = info;
     parent.appendChild(child);
 }
 
-addDOM(parent, "h1", candidate.name)
-addDOM(parent, "h3", candidate.district)
-addDOM(parent, "h3", candidate.bio);
-addDOM(parent, "h3", candidate.mission);
+// elements added to dom, occaisionally setting attribute references to give newly created elements an ID for later referral
+addDOM("h1", candidate.name)
+addDOM("h3", candidate.district)
+addDOM("h3", candidate.bio);
+addDOM("h3", candidate.mission);
+
+addDOM("h2", "The Issues");
+addDOM("ul", "");
+child.setAttribute("id", "issue");
+addDOM("h2", "Volunteer Info")
+addDOM("ul", "");
+child.setAttribute("id", "volunteers")
+addDOM("h2", "Events")
+addDOM("ul", "");
+child.setAttribute("id", "events")
+
+addDOM("img", images.headShot);
+addDOM("img", images.familyPhoto);
+addDOM("img", images.constituents);
+
+addDOM("a", "Register to Vote");
+child.setAttribute("href", links.register)
+addDOM("a", "Donate");
+child.setAttribute("href", links.donate)
+
+
+parent = document.querySelector("#issue");
+addDOM("li", platform.taxes);
+addDOM("li", platform.jobs);
+addDOM("li", platform.infrastructure);
+addDOM("li", platform.health);
+addDOM("li", platform.Crime);
+
+
+// Loops required for object arrays
+parent = document.querySelector("#volunteers")
+for (let i = 0; i < volunteers.length; i++) { 
+    addDOM("h2", volunteers[i].name);
+    addDOM("li", volunteers[i].address);
+    addDOM("li", volunteers[i].email);
+    addDOM("li", volunteers[i].phone);
+    addDOM("li", volunteers[i].availability);
+    addDOM("li", volunteers[i].activities);
+}
+
+parent = document.querySelector("#events")
+for (let i = 0; i < events.length; i++) { 
+    addDOM("h2", events[i].name);
+    addDOM("li", events[i].date);
+    addDOM("li", events[i].location);
+}
 
